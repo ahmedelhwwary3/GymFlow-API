bfd8803c474d8d6d6f075f2dab4422b2c0f117e2
Ahmed Elhwwary <ahmed.elhwwary3@gmail.com>
2026-06-06 23:52:18 +0300

    Add login endpoint

---
+++ GymManagementAPI/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceTier;
+using auth= RepositoryTier.DTOs.Authentication;
+
+namespace GymManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+        private readonly IUserService _userService;
+        public AuthController(
+            IUserService userService,
+            ILogger<AuthController> logger)
+        {
+            _userService = userService;
+            _logger = logger;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(auth.LoginRequest request)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            var response =await _userService.LoginAsync(request);
+            if(response==null)
+                return Unauthorized("Invalid email or password.");
+
+            return Ok(response);
+        }
+
+
+    }
+}
---
+++ ServiceTier/API Configurations/JWTOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceTier.Configurations
+{
+    public class JWTOptions
+    {
+        public string Issuer { get; set; } = ""; 
+        public string Audience { get; set; } = "";
+        public int ExpirationInMinutes { get; set; }
+        public int RefreshTokenExpirationInDays { get; set; }
+    }
+}
