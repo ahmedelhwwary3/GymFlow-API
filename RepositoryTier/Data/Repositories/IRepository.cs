7333a69adf416e3bb11608442244aa68a24729ae
Ahmed Elhwwary <ahmed.elhwwary3@gmail.com>
2026-06-06 17:53:40 +0300

    Add Repositories Generic Repo for Modify & Select Object & custom repos for complex queries.

---
+++ RepositoryTier/Data/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryTier.Data.Repositories
+{
+    internal interface IRepository<T> where T : class
+    { 
+        Task<T?> GetByIdAsync(int Id,params Expression<Func<T, object>>[]includes);
+        Task<T?> FindByIdAsync(int Id);
+        Task DeleteByIdAsync(int Id);
+        Task AddAsync(T entity);
+        Task<Boolean> ExistsByIdAsync(int Id);
+        Task SaveChangesAsync();
+    }
+}
---
+++ RepositoryTier/Data/Repositories/Repository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryTier.Data.Repositories
+{
+    internal class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly GymManagementDbContext _context;
+
+        public Repository(GymManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+        }
+
+        public async Task DeleteByIdAsync(int Id)
+        {
+            var entity = await FindByIdAsync(Id);
+            if(entity != null)
+                _context.Remove(entity);
+        }
+
+        public async Task<T?> FindByIdAsync(int Id)
+        {
+            return await _context.FindAsync<T>(Id);
+        }
+
+        public async Task<T?> GetByIdAsync
+            (int Id, params Expression<Func<T, object>>[] includes)
+        {
+            var query = _context.Set<T>()
+                .Where(e => EF.Property<int>(e, "Id") == Id);
+
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Boolean> ExistsByIdAsync(int Id)
+        {
+            return await _context.Set<T>()
+                .AnyAsync(e=>EF.Property<int>(e,"Id") == Id);
+        }
+    }
+}
